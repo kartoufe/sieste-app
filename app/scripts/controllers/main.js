40a4dad2c60612e0231d8bc1dded7c81b0458aa3
raouf <bengamra.raouf7@gmail.com>
2017-06-10 18:25:43 +0200

    initialisation angularJS

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siesteAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the siesteAppApp
+ */
+angular.module('siesteAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
